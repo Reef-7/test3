ad097ee36e0f09b15538aba34c5b571a0252b306
Reef-7 <Rifstudy7@gmail.com>
2023-06-19 19:42:56 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3070;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(port, function () {
+    console.log('connected succesfully')
+})
+
+app.get('/', (req, res) => {
+    //res.send('hi');
+    res.sendFile(path.join(__dirname, 'home.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+})
+
+app.get('/Products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Products.html'));
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+})
+app.get('/sale', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sale.html'));
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+})
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'cart.html'));
+})
+
+

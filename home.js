8004be566b197114f20ccc91fc25003e3f220d58
Reef-7 <Rifstudy7@gmail.com>
2023-06-27 23:08:28 +0300

    Homepage update etc

--- home.js
+++ home.js
@@ -16,37 +16,11 @@ function make_base() {
 
 
 
-$(document).ready(function () {
-    $(".btn-outline-dark").click(function () {
-        // Get the position of the clicked button
-        var buttonPosition = $(this).offset();
 
-        // Create a clone of the image and append it to the body
-        var image = $(this).closest(".card").find(".card-img-top");
-        var imageClone = image.clone().addClass("animate");
-        imageClone.attr("id", "flyingImage");
-        $("body").append(imageClone);
 
-        // Position the flying image at the clicked button's position
-        $("#flyingImage").css({
-            top: buttonPosition.top + "px",
-            left: buttonPosition.left + "px"
-        });
-
-        // Animate the flying image to the cart logo position
-        $("#flyingImage").animate(
-            {
-                top: $(".bi-cart4").offset().top + "px",
-                left: $(".bi-cart4").offset().left + "px",
-                width: "20px",
-                height: "20px"
-            },
-            1000,
-            function () {
-                // Remove the flying image after animation
-                $(this).remove();
-            }
-        );
-    });
+$(window).on('scroll', function () {
+    var scrollPosition = $(this).scrollTop();
+    $('.top-banner').css('transform', 'translateY(' + scrollPosition * 0.5 + 'px)');
 });
+
 
